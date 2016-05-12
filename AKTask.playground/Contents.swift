51e68dc953df0dce2c9bd0e497eeaccfae4ee312
akio0911 <akio0911@gmail.com>
2016-05-13 02:01:43 +0900

    Add initClosure

---
+++ AKTask.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+class Task {
+    typealias InitClosure = () -> Void
+    
+    init(initClosure: InitClosure) {
+        initClosure()
+    }
+}
+
+print("before")
+let task1 = Task(initClosure: { print("closure") })
+print("after")
+
